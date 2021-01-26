6b5c2b2642ca1516bdd7d3e45f57877f8eeba321
Aaron Utterback <Autterback3@mail.greenriver.edu>
2021-01-26 15:47:47 -0500

    added route with params

--- index.php
+++ index.php
@@ -23,11 +23,44 @@ $f3->route('GET /breakfast', function(){
     echo $view->render('/views/breakfast.html');
 });
 
-// define a breakfast route
+// define a lunch route
 $f3->route('GET /lunch', function(){
     // echo "breakfast";
     $view = new Template();
     echo $view->render('/views/lunch.html');
+});
+
+// Define a lunch/sandwich route
+$f3->route('GET /lunch/sandwich', function(){
+    // echo "breakfast";
+    $view = new Template();
+    echo $view->render('/views/sandwich.html');
+});
+
+// Parameter in anonymous functions
+$f3->route('GET /breakfast/@item', function($f3, $params){
+    // echo "breakfast";
+    var_dump($params);
+    $menu = array('eggs', 'waffles', 'pancakes');
+    $item = $params['item'];
+    if(in_array($item, $menu)){
+        switch($item){
+            case 'eggs';
+                $view = new Template();
+                echo $view->render('/views/eggs.html');
+            case 'pancakes';
+                echo "Swedish or American?";
+            case 'waffles';
+                $f3->reroute("https://wafflehouse.com");
+            default:
+                $f3->error(404);
+        }
+    } else{
+        echo "sorry we dont serve $item";
+    }
+
+    $view = new Template();
+    echo $view->render('/views/breakfast.html');
 });
 
 // Run fat free
